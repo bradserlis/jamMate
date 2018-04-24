a3fa0deb99afbbfa3fe47997371ca31b67f7cd4a
bradserlis <bradserlis@yahoo.com>
2018-04-24 14:06:33 -0700

    call to retrieve zip codes within 10 miles returns array properly, cannot properly compare to db and return matches

--- controllers/profile.js
+++ controllers/profile.js
@@ -5,9 +5,10 @@ const 	mongoose = require('mongoose'),
 		request = require('request'),
 		profileRoute = express.Router();
 
+
 const User = require('../models/user');
 
-
+var allZips = [];
 profileRoute.get('/', isLoggedIn, function(req, res){
 	res.render('profile');
 });
@@ -17,13 +18,56 @@ profileRoute.get('/search', isLoggedIn, function(req, res){
 		if(err){
 			return console.log(err);
 		} 
-		console.log(body)
-	})	
-	res.render('search');
-});
+		//made the json call into an object
+		let bodyObj = JSON.parse(body);
+		let matchedUsers = [];
+		let i = 0;
+		bodyObj.zip_codes.forEach(function(zip){
+			zip = Number(zip);
+			User.find({zipcode: zip}, function(err, match){
+				if(err){
+					return console.log('it broke');
+				} 
+				console.log(match)
+				match.forEach(function(element){
+				// if(element !== {}){
+				// 	matchedUsers.push(element);
+				// }
+				matchedUsers.push(i)
+				i++;
+				})
+			})
+		})
+		console.log('matchedUsers looks like this:', matchedUsers);
 
-function calledZipcodes(val){
-	return val;
-}
+	})
+})
+
+	// for each of the array of zipcodes, do user.find, match user zipcode to the current item of the foreach loop
+// if it matches, push to the new user array of objects
+// render that array onto page
+
+// 		//send back all users
+// 		User.find({}, function(err, val){
+// 		//send back only the zipcodes from those users
+// 			val.forEach( (item) =>{
+// 			//store the zipcode for moving to array	
+// 				let currZip = item.zipcode;
+// 			//push the value into the array, and repeat until through all users
+// 				allZips.push(currZip);
+// 			})
+// 		});
+// 		console.log('allzips looks like this:', allZips);
+
+// 		// const matchedZip = bodyObj.zip_codes.filter(function(value){
+// 		// 	allZips.forEach( (zip) => {
+// 		// 		return value == zip;
+// 		// 	});
+// 		})
+// 		console.log(matchedZip);
+// 	})
+// 	res.render('search');
+// })	
+
 
 module.exports = profileRoute;
